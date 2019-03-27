b9ad3ba447597ca8e01b5b5c23fcc8b07539b0e6
r <riku.makynen@student.oulu.fi>
2019-03-27 16:58:48 +0200

    Calc V2

--- Calc4/app/src/main/java/tol/oulu/fi/MainActivity.java
+++ Calc4/app/src/main/java/tol/oulu/fi/MainActivity.java
@@ -1,5 +1,6 @@
 package tol.oulu.fi;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,7 +11,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener
 {
@@ -18,6 +24,70 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
     protected int addThis = 0;
     protected int internalMemory = 0;
     protected TextView totalSum;
+//Restore & save state
+
+    /*
+    saveState() from wiki.oulu.fi/display/esde/2019/03/27/Some+help+to+Calculator+exercise     */
+    private final static String RESULT = "savedata";
+
+    private void saveState()
+    {
+        FileOutputStream outputStream;
+        try
+        {
+            outputStream = openFileOutput(RESULT, Context.MODE_PRIVATE);
+            byte[] bytes = new byte[8];
+            ByteBuffer.wrap(bytes).putInt(bigInt);
+            outputStream.write(bytes);
+            outputStream.close();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    restoreState() from wiki.oulu.fi/display/esde/2019/03/27/Some+help+to+Calculator+exercise     */
+    private void restoreState()
+    {
+        FileInputStream inputStream = null;
+        try
+        {
+            inputStream = openFileInput(RESULT);
+            byte[] bytes = new byte[8];
+            inputStream.read(bytes);
+            bigInt = (ByteBuffer.wrap(bytes).getInt());
+            inputStream.close();
+
+            totalSum = (TextView) findViewById(R.id.totalSum);
+            String temp_str = String.valueOf(bigInt);
+            totalSum.setText(temp_str);
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        restoreState();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        saveState();
+    }
+
 
     //Options
     @Override
